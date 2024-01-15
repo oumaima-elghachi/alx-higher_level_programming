65013a8773807e9d279680abd06c881afc742c56
oumaima <elghachiomaima1@gmail.com>
2024-01-15 12:37:02 +0100

    test

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""A script lists all State objects from the database hbtn_0e_6_usa"""
-
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        print("{:}: ".format(session.query(State).order_by(
-            State.id).first().id), end="")
-        print(session.query(State).order_by(State.id).first().name)
-    except:
+    instance = session.query(State).first()
+    if instance is None:
         print("Nothing")
-    session.close()
+    else:
+        print(instance.id, instance.name, sep=": ")
